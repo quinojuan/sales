963dd0c2e679314d342bcf0f5abbe0014db54756
Juan Quino <quinojuan@gmail.com>
2023-06-18 12:48:59 -0300

    auth

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
 export const login = (req: Request, res: Response) => {
-  res.json("LOGIN");
+  res.json("LOGIN!");
 };
 
 export const generateCode = (req: Request, res: Response) => {
-  res.json("GENERATE CODE");
+  res.json("GENERATE CODE!");
 };
